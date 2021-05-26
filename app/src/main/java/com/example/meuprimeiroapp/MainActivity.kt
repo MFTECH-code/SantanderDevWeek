7a67856cdcf2ecf433994564e4b1369b8ec7bb7d
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-05-26 11:32:37 -0300

    Construção do menu, e ações do menu

--- app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
@@ -2,47 +2,39 @@ package com.example.meuprimeiroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // DEC variáveis
-            // val -> não muda (constante)
-            // var -> varía (variável)
-        var contador : Int = 10;
-        contador = 23;
-
-        val contador2 : Int = 1;
-        // contador2 = 3; // Não compila
-
-        // Segurança nula
-        val languageName : String? = null;
-
-        // Condicionais
-        val idade : Int = 20;
-        /*
-        val maioridade : String = if (idade > 18) {
-            "Você é maior de idade";
-        } else {
-            "Você é menor de idade";
-        }
-        */
-
-        // when
-        val minhaIdade = when {
-            idade > 18 -> "Você é maior de idade"
-            else -> "Você é menor de idade"
-        }
-
-        // Funções
-        fun minhaIdade() :String = "Minha idade é 18"
-
-        minhaIdade();
-
-        // Instanciando uma classe
-        val medicamento = Medicamento("teste 1", "2 vezes ao dia");
-        medicamento.formula
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Chamando nosso layout
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    // Escutando o item do menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Quando o item for apertado faça alguma ação
+        return when (item.itemId) {
+            R.id.item1 -> {
+                Log.d("CLICK", "CLICK NO ITEM 1") // log no console
+                true
+            }
+            R.id.item2 -> {
+                Log.d("CLICK", "ITEM 2 SELECIONADO KKKKSJJJ")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
--- app/src/main/java/com/example/meuprimeiroapp/Medicamento.kt
+++
@@ -1,6 +0,0 @@
-package com.example.meuprimeiroapp
-
-class Medicamento(val formula : String, val posologia : String) {
-
-
-}
